6b5d526d7921868fdd4e5c6e57bebb0fbb31a69c
h4rm41n <mail.ahmad.harmain@gmail.com>
2016-09-26 17:25:35 +0700

    Commit awal

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express')();
+var app = require('http').Server(express);
+var socket = require('socket.io')(app);
+var five = require('johnny-five');
+var board = new five.Board();
+
+app.listen(5000, function(){
+	console.log('Server is running...');
+});
+
+express.get('/', function(req, res){
+	res.sendFile(__dirname+'/index.html');
+});
+
+board.on('ready', function(){
+	console.log('Board is ready!');
+	var led = new five.Led(13);
+
+	this.repl.inject({
+		led:led
+	});
+
+	//led.blink(50);
+
+    socket.on('connection', function(socket){
+    	// socket.emit('cuap', 'data-ku-ini');
+		socket.on('on', function(){
+			console.log("on");
+			led.on();
+		});
+
+		socket.on('off', function(){
+			console.log("off");
+			led.off();
+		});
+    });
+})
